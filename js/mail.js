ad51b10ecca4710aa676fe6a84900a1e3f2abcea
Nolly programmer <ghasttankix@gmail.com>
2026-03-25 01:15:56 +0300

    Initial commit: Nolly Building Base

---
+++ js/mail.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Инициализация библиотеки AOS
+    AOS.init({
+        duration: 1200, // Скорость анимации
+        once: false,    // Срабатывает при каждом скролле (вверх и вниз)
+    });
+});
